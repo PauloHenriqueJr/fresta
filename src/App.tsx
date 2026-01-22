be47f12b18274d0df844e5646bfba1aefa92ced5
Paulo Henrique de Jesus <Paulojack2011@gmail.com>
2026-01-22 18:19:14 -0300

    add fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CalendarView from "./pages/CalendarView";
 import Onboarding from "./pages/Onboarding";
@@ -74,7 +74,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Gateway />} />
             <Route path="/home" element={<LandingPage />} />
@@ -172,7 +172,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </GlobalSettingsProvider>
   </QueryClientProvider>
